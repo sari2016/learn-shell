ac3dfd120d95d02ad6873785392b3fc145562bee
sarita1104 <saritagovindaraju83@gmail.com>
2023-09-21 09:51:31 -0500

    expense scripts

---
+++ print.sh
@@ -0,0 +1,8 @@
+echo hello world
+
+echo -e "\e[31mRED COLOR\e[0m"
+echo -e "\e[32mGREEN COLOR\e[0m"
+echo -e "\e[33mYELLOW COLOR\e[0m"
+echo -e "\e[34mBLUE COLOR\e[0m"
+echo -e "\e[35mMAGENTA COLOR\e[0m"
+echo -e "\e[36mCYAN COLOR\e[0m"
